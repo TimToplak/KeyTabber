f13dfd0f477316a8083872e18857338fc0e53833
TimMit <tim.toplak@hotmail.com>
2019-09-19 20:06:58 +0200

    youtube added some space, so margin is needed

--- contentScriptReddit.js
+++ contentScriptReddit.js
@@ -3,6 +3,16 @@ console.log("KeyTabber running");
 var aElemets;
 var i = -1;
 
+function eventFire(el, etype) {
+  if (el.fireEvent) {
+    el.fireEvent("on" + etype);
+  } else {
+    var evObj = document.createEvent("Events");
+    evObj.initEvent(etype, true, false);
+    el.dispatchEvent(evObj);
+  }
+}
+
 document.addEventListener("keydown", logKey);
 function logKey(e) {
   if (e.which == 9) {
@@ -24,6 +34,14 @@ function logKey(e) {
 
     aElemets[i].focus();
     aElemets[i].classList.add("boxshadow");
+  }
+
+  console.log(e.which);
+  if (e.which == 38 || e.which == 40) {
+    console.log("firing");
+    //eventFire(document.querySelector(".yk4f6w-0.cCMdtd"), "click");
+    console.log(document.querySelector(".yk4f6w-0.cCMdtd"));
+    //.focus();
   }
 }
 
