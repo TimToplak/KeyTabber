44ecc78c8e5437cf70f8293a21b7361237175c91
TimMit <tim.toplak@hotmail.com>
2020-10-11 18:37:06 +0200

    new class for google link hits

--- contentScriptGoogle.js
+++ contentScriptGoogle.js
@@ -1,13 +1,13 @@
-console.log("KeyTabber running");
+console.log('KeyTabber running');
 
 var aElemets;
 var i = -1;
 
-document.addEventListener("keydown", logKey);
+document.addEventListener('keydown', logKey);
 function logKey(e) {
   if (e.which == 9) {
     e.preventDefault();
-    aElemets = document.querySelectorAll("[class='r']");
+    aElemets = document.querySelectorAll("[class='yuRUbf']");
     console.log(aElemets);
     removeClass();
     if (e.shiftKey) {
@@ -22,13 +22,13 @@ function logKey(e) {
       }
     }
 
-    aElemets[i].querySelector("a").focus();
-    aElemets[i].querySelector("a").classList.add("boxshadow");
+    aElemets[i].querySelector('a').focus();
+    aElemets[i].querySelector('a').classList.add('boxshadow');
   }
 }
 
 function removeClass() {
   try {
-    aElemets[i].querySelector("a").classList.remove("boxshadow");
-  } catch (e) { }
+    aElemets[i].querySelector('a').classList.remove('boxshadow');
+  } catch (e) {}
 }
